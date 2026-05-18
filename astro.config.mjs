c301e1609b89449b9b29aca28653fe047a198165
Seojin Kim <rlatjwls7882@gmail.com>
2026-05-18 10:29:47 +0900

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://rlatjwls7882.github.io',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   integrations: [mdx()],
 });
