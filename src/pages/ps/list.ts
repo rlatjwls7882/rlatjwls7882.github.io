18d8299f2a1cb71919bb885b15296863d6342d8d
Seojin Kim <rlatjwls7882@gmail.com>
2026-06-03 12:27:54 +0900

    .

--- src/pages/ps/list.ts
+++ src/pages/ps/list.ts
@@ -1,47 +1,52 @@
 export const list = [
   {
-    category: '자료구조',
-    title: 'Vector',
-    slug: 'vector',
-  },
-  {
-    category: '정렬',
-    title: 'Sort',
-    slug: 'sort',
-  },
-  {
-    category: '자료구조',
-    title: 'Stack',
-    slug: 'stack',
-  },
-  {
-    category: '자료구조',
-    title: 'Queue',
-    slug: 'queue',
-  },
-  {
-    category: '자료구조',
-    title: 'Deque',
-    slug: 'deque',
-  },
-  {
-    category: '자료구조',
-    title: 'Set',
-    slug: 'set',
-  },
-  {
-    category: '자료구조',
-    title: 'Map',
-    slug: 'map',
-  },
-  {
-    category: '자료구조',
-    title: 'Priority Queue',
-    slug: 'priority_queue',
-  },
-  {
-    category: '자료 구조',
-    title: 'DSU (Disjoint Set Union)',
-    slug: 'dsu',
+    title: '기초 알고리즘',
+    items: [
+      {
+        category: '자료 구조',
+        title: 'Vector',
+        slug: 'vector',
+      },
+      {
+        category: '정렬',
+        title: 'Sort',
+        slug: 'sort',
+      },
+      {
+        category: '자료 구조',
+        title: 'Stack',
+        slug: 'stack',
+      },
+      {
+        category: '자료 구조',
+        title: 'Queue',
+        slug: 'queue',
+      },
+      {
+        category: '자료 구조',
+        title: 'Deque',
+        slug: 'deque',
+      },
+      {
+        category: '자료 구조',
+        title: 'Set',
+        slug: 'set',
+      },
+      {
+        category: '자료 구조',
+        title: 'Map',
+        slug: 'map',
+      },
+      {
+        category: '자료 구조',
+        title: 'Priority Queue',
+        slug: 'priority_queue',
+      },
+      {
+        category: '자료 구조',
+        title: 'DSU (Disjoint Set Union)',
+        slug: 'dsu',
+      },
+    ],
   },
 ] as const
