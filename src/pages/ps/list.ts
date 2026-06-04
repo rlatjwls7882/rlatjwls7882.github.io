afe5840869d5453cd61b44aeb04110db3d06f150
Seojin Kim <rlatjwls7882@gmail.com>
2026-06-04 22:29:05 +0900

    .

--- src/pages/ps/list.ts
+++ src/pages/ps/list.ts
@@ -3,57 +3,46 @@ export const list = [
     title: '기초 알고리즘',
     items: [
       {
-        category: '자료 구조',
         title: 'Vector',
         slug: 'vector',
       },
       {
-        category: '정렬',
         title: 'Sort',
         slug: 'sort',
       },
       {
-        category: '탐색',
         title: 'Binary Search (이분 탐색)',
         slug: 'binary_search',
       },
       {
-        category: '구간 처리',
         title: 'Prefix Sum (누적 합)',
         slug: 'prefix_sum',
       },
       {
-        category: '탐색',
         title: 'Two Pointer (두 포인터)',
         slug: 'two_pointer',
       },
       {
-        category: '자료 구조',
         title: 'Stack',
         slug: 'stack',
       },
       {
-        category: '자료 구조',
         title: 'Queue',
         slug: 'queue',
       },
       {
-        category: '자료 구조',
         title: 'Deque',
         slug: 'deque',
       },
       {
-        category: '자료 구조',
         title: 'Set',
         slug: 'set',
       },
       {
-        category: '자료 구조',
         title: 'Map',
         slug: 'map',
       },
       {
-        category: '자료 구조',
         title: 'Priority Queue (우선순위 큐)',
         slug: 'priority_queue',
       },
@@ -63,7 +52,6 @@ export const list = [
     title: '중급 알고리즘',
     items: [
       {
-        category: '자료 구조',
         title: 'DSU (Disjoint Set Union, 분리 집합)',
         slug: 'dsu',
       },
@@ -73,7 +61,6 @@ export const list = [
     title: '고급 알고리즘',
     items: [
       {
-        category: '자료 구조',
         title: 'PBDS (Ordered Set)',
         slug: 'pbds',
       },
